b0da1beaa80647b449f2c27b5f82867d81bb4482
dm <dm>
1999-08-08 13:14:11 +0000

    Cleanup, CPPFLAGS verwenden

---
+++ include/os-bsdi/sysdep.h
@@ -0,0 +1,81 @@
+/* $Id$ */
+/*
+ *  UNIX-Connect, a ZCONNECT(r) Transport and Gateway/Relay.
+ *  Copyright (C) 1993-94  Martin Husemann
+ *  Copyright (C) 1995     Christopher Creutzig
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * ------------------------------------------------------------------------
+ * Eine deutsche Zusammenfassung zum Copyright dieser Programme sowie der
+ * GNU General Public License finden Sie in der Datei README.1st.
+ * ------------------------------------------------------------------------
+ *
+ *  Bugreports, suggestions for improvement, patches, ports to other systems
+ *  etc. are welcome. Contact the maintainer by e-mail:
+ *  christopher@nescio.foebud.org or snail-mail:
+ *  Christopher Creutzig, Im Samtfelde 19, 33098 Paderborn
+ *
+ *  There is a mailing-list for user-support:
+ *   unix-connect@mailinglisten.im-netz.de,
+ *  write a mail with subject "Help" to
+ *   nora.e@mailinglisten.im-netz.de
+ *  for instructions on how to join this list.
+ */
+
+
+/*
+ *  UNIX-Connect: sysdep.h
+ *
+ *  Systemabhaengige C Anweisungen.
+ *
+ *  Alle fuer dieses System gueltigen #define's sollten hier gesetzt
+ *  werden, alle nicht von System-Headern bereitgestellten Prototypen
+ *  gehoeren hier hin.
+ *
+ */
+
+#ifndef SYSDEP_H
+#define SYSDEP_H
+
+#define	OS	"UNIX"
+#define	BRAND	"BSDI BSD/386"
+
+#ifndef BSD	/* param.h */
+#define BSD	/* beliebiges BSD, auch z.B. NeXT */
+#endif
+
+#define HAS_STRING_H /* Die str... Funktionen sind in <string.h> */
+#define HAS_SYS_FCNTL_H
+
+#define HAS_POSIX_TERMIOS
+
+#define LEAVE_CTRL_TTY
+
+/* man weiss nie... */
+#include <limits.h>
+#define MAXINT INT_MAX
+
+/*
+ *  Die Online-Empfangsprogramme koennen ohne Passwortabfrage des
+ *  Systems eingebunden werden, sie fragen daher selbst nach einem
+ *  Passwort:
+ */
+
+#define	ASK_PASSWD
+
+
+#endif
+
---
+++ include/os-sunos/sysdep.h
@@ -0,0 +1,89 @@
+/* $Id$ */
+/*
+ *  UNIX-Connect, a ZCONNECT(r) Transport and Gateway/Relay.
+ *  Copyright (C) 1993-94  Martin Husemann
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * ------------------------------------------------------------------------
+ * Eine deutsche Zusammenfassung zum Copyright dieser Programme sowie der
+ * GNU General Public License finden Sie in der Datei README.1st.
+ * ------------------------------------------------------------------------
+ *
+ *  Bugreports, suggestions for improvement, patches, ports to other systems
+ *  etc. are welcome. Contact the maintainer by e-mail:
+ *  christopher@nescio.foebud.org or snail-mail:
+ *  Christopher Creutzig, Im Samtfelde 19, 33098 Paderborn
+ *
+ *  There is a mailing-list for user-support:
+ *   unix-connect@mailinglisten.im-netz.de,
+ *  write a mail with subject "Help" to
+ *   nora.e@mailinglisten.im-netz.de
+ *  for instructions on how to join this list.
+ */
+
+
+/*
+ *  UNIX-Connect: sysdep.h
+ *
+ *  Systemabhaengige C Anweisungen.
+ *
+ *  Alle fuer dieses System gueltigen #define's sollten hier gesetzt
+ *  werden, alle nicht von System-Headern bereitgestellten Prototypen
+ *  gehoeren hier hin.
+ *
+ */
+
+#ifndef SYSDEP_H
+#define SYSDEP_H
+
+#define	OS	"UNIX"
+#define	BRAND	"SunOS4"
+
+#define SVR4	/* SYS5 auch setzen! */
+#define SYSV	/* beliebiges System V */
+
+#define HAS_STRING_H		/* Die str... Funktionen sind in <string.h> */
+#define HAS_STRINGS_H           /* fuer index() */
+/* #define HAS_POSIX_TERMIOS */	/* benutze <termios.h> IOCTL's */
+#define HAS_SYSV_TERMIO
+#define NEED_CRTSCTS		/* Flag in termio */
+#define HAS_UNISTD_H            /* Brauchen wir eben */
+#define MAJOR_IN_SYSMACROS	/* major()/minor() in sys/sysmacros.h */
+/* #define	NEED_MODE_H		 inlcude mode.h fuer open-modes */
+#define NEED_VALUES_H		/* include values.h */
+#define FILENAME_MAX 255	/* So ist es in sys/dir.h auch definiert,
+					aber als MAXNAMLEN */
+
+#define LEAVE_CTRL_TTY		/* Entlasse das Controlling TTY... */
+/* #define USE_SETSID		 ... durch einen Aufruf von setsid(2) */
+#define USE_SETPGRP		/* ... durch einen Aufruf von setpgrp(2) */
+#define HAS_SYS_FCNTL_H
+#define SPRINTF_RETURNS_S    /* sprintf(s,...) returns s anstatt der
+				Anzahl der geschriebenen Zeichen */
+
+/*
+ *  Die Online-Empfangsprogramme koennen ohne Passwortabfrage des
+ *  Systems eingebunden werden, sie fragen daher selbst nach einem
+ *  Passwort:
+ */
+
+#define	ASK_PASSWD
+
+/* Das haben wir sonst nicht in SunOS 4 : */
+#define strerror(x) sys_errlist[x]
+char *sys_errlist[];
+
+#endif
