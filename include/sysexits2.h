224555ea00bb554d38468351c99013337095aaaa
dinoex <dinoex>
1999-09-21 04:15:49 +0000

    Fuer OS2

---
+++ include/sysexits2.h
@@ -0,0 +1,52 @@
+/* $Id$ */
+/*
+ *  UNIX-Connect, a ZCONNECT(r) Transport and Gateway/Relay.
+ *  Copyright (C) 1999     Dirk Meyer
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * ------------------------------------------------------------------------
+ * Eine deutsche Zusammenfassung zum Copyright dieser Programme sowie der
+ * GNU General Public License finden Sie in der Datei README.1st.
+ * ------------------------------------------------------------------------
+ *
+ *  Bugreports, suggestions for improvement, patches, ports to other systems
+ *  etc. are welcome. Contact the maintainer by e-mail:
+ *  dirk.meyer@dinoex.sub.org or snail-mail:
+ *  Dirk Meyer, Im Grund 4, 34317 Habichstwald
+ *
+ *  There is a mailing-list for user-support:
+ *   unix-connect@mailinglisten.im-netz.de,
+ *  write a mail with subject "Help" to
+ *   nora.e@mailinglisten.im-netz.de
+ *  for instructions on how to join this list.
+ */
+
+#ifdef HAVE_SYSEXITS
+
+#include <sysexits.h>
+
+#else
+
+#define EX_OK			0
+#define EX_TEMPFAIL		75
+#define EX_CONFIG		78
+#define EX_DATAERR		65
+#define EX_IOERR		74
+#define EX_CANTCREAT		73
+#define EX_USAGE		64
+
+#endif
+
